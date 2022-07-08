df62463d2edafcc65e4072325f4a585b8516091b
Af0ns0Castro <108541617+Af0ns0Castro@users.noreply.github.com>
2022-07-08 01:49:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+//
+ //console.log('Bem vindo ao starter!!!')
+ //console.log('Não ignore')
+//
+// console.log({
+//    name: "Afonso",
+//    idade: 29,
+//    andar: function() {
+//        console.log('andar')
+//
+//    }
+// })
+//
+// console.log([
+//    "Ovos",
+//    "Leite",
+//    "Acucar",
+//    "Sal",
+//    
+//
+//])
+
+//   let name, age, isHuman
+//
+ //     name = "Plutão"
+//      age = 1
+ //     isHuman = false
+//
+//      console.log(`O ${name} tem ${age} de idade e é humano? ${isHuman}`)
+
+//var animals = [
+//   {  name: "Plutão",
+//      raca: "PitBull",
+//      age: 1,
+//      color: "Branco",
+//      patas: 4,
+//      chato: true
+//      
+//   },
+//   {  name: "Honda",
+//      raca: 'PitBull',
+//      age: 5,
+//      color: 'Branco com Marron',
+//      patas: 4,
+//      chato: false
+//      
+//   },
+//   {  name: "Odin",
+ ///     raca: "indefinida",
+   //   age: 5,
+//      color: "Marron",
+//      patas: 4,
+//      chato: false
+//      
+// //  },
+// /  {  name: "Luna",
+//      raca: "Dalmata",
+//      age: 4,
+//      color: "Branco com pintas pretas",
+//      patas: 4,
+//      chato: false,
+//      
+//   },
+//   {  name: "Mel",
+//      raca: "Chowchow",
+//      age: 12,
+//      color: "Preto",
+//      patas: 4,
+//      chato: false,
+//      
+//   },
+//   {  name: "Rex",
+//      raca: "Viralata",
+//      age: 6,
+//      color: "Preto",
+//      patas: 4,
+///      chato: true,
+ //     
+//   }
+//]
+//      console.log(`O ${animals[5].name} é um ${animals[5].raca} com ${animals[5].age} anos e da cor ${animals[5].color} tem ${animals[5].patas} patas e é chato? ${animals[5].chato}.`)
+
+let weight
+
+console.log(typeof weight)
+
+let name = "Afonso"
+let age = 29
+let stars = 4.8
+let isSubscribed = true
